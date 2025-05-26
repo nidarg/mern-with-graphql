2641df84dbf0c58a3cb59c95dd0df75a8f069443
Gradin <gradin_g@yahoo.com>
2025-05-26 19:28:17 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'; // yarn add express
+import { createHandler } from 'graphql-http/lib/use/express';
+import dotenv from 'dotenv'
+import { altairExpress } from 'altair-express-middleware';
+import schema from './schema/schema.js'
+import colors from 'colors'
+import { connectDB } from './config/db.js';
+
+dotenv.config()
+// Create an express instance serving all methods on `/graphql`
+// where the GraphQL over HTTP express request handler is
+
+const app = express();
+// connect to db
+connectDB(); 
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+ app.use('/graphql', createHandler({ schema }));
+
+ app.use('/altair', altairExpress({
+    endpointURL:"/graphql"
+ }))
+
+app.listen(port, () => {
+  console.log(`🚀 GraphQL ready at http://localhost:${port}/graphql`);
+  console.log(`🌈 Altair GraphQL UI at http://localhost:${port}/altair`);
+});
---
+++ server/models/Client.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ClientSchema = new mongoose.Schema({
+    name:{
+        type: String,
+    },
+     email:{
+        type: String,
+    },
+     phone:{
+        type: String,
+    },
+});
+const Client = mongoose.model('Client',ClientSchema)
+export default Client
